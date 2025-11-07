f890ecee956152fe99939416fa9a60c542b49259
Matt-Owen-81 <matt_r_owen@hotmail.com>
2025-11-07 22:06:39 +0000

    Update generate_diagram.py

--- generate_diagram.py
+++ generate_diagram.py
@@ -199,5 +199,4 @@ for header, sub_map in grouped.items():
     drawio_root.append(diagram_element)
 
 # Save to file
-pretty_xml = minidom.parseString(tostring(drawio_root)).toprettyxml(indent="  ")
-with open('diagram.drawio', 'w',
+pretty_xml = minidom.parseString(tostring(drawio_root)).toprettyxml(indent="
